b588ca05a12f2c442835dd6afca6b0b59f10a192
Brunner20 <43754656+Brunner20@users.noreply.github.com>
2021-01-05 20:06:22 +0300

    update service

--- src/main/java/by/tc/task03/service/NodeBuilder.java
+++ src/main/java/by/tc/task03/service/NodeBuilder.java
@@ -1,10 +1,10 @@
 package by.tc.task03.service;
 
-import by.tc.task03.dao.exception.DaoException;
+import by.tc.task03.service.exception.ServiceException;
 import by.tc.task03.entity.Node;
 
 
 public interface NodeBuilder {
 
-    Node parseXML(String path) throws DaoException;
+    Node parseXML(String path) throws ServiceException;
 }
--- src/main/java/by/tc/task03/service/impl/NodeBuilderImpl.java
+++ src/main/java/by/tc/task03/service/impl/NodeBuilderImpl.java
@@ -1,14 +1,25 @@
 package by.tc.task03.service.impl;
 
 import by.tc.task03.dao.XMLParser;
-import by.tc.task03.dao.exception.DaoException;
+import by.tc.task03.dao.XMLReader;
+import by.tc.task03.dao.impl.XMLParserImpl;
+import by.tc.task03.service.exception.ServiceException;
 import by.tc.task03.entity.Node;
 import by.tc.task03.service.NodeBuilder;
+
+import java.io.IOException;
 
 
 public class NodeBuilderImpl implements NodeBuilder {
 
-    public Node parseXML(String path) throws DaoException {
-        return  XMLParser.parseXML(path);
+    public Node parseXML(String path) throws ServiceException {
+
+        XMLParser parser;
+        try {
+            parser = new XMLParserImpl(XMLReader.readXML(path));
+        } catch (IOException e) {
+            throw  new ServiceException(e.fillInStackTrace());
+        }
+        return  parser.parseXML();
     }
 }
